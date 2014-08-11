c6a23c7df23c8eb8055e1a36633448e44bcd5846
Luke Bonaccorsi <luke@lukeb.co.uk>
2014-08-11 22:10:21 +0100

    Refactor to fit with new method for initing plugins

--- index.js
+++ index.js
@@ -1,43 +1,50 @@
 http = require('http');
 
 module.exports = function(api) {
-    api.listen('is (.+?) up\\?', function(from, interface, params){
-        api.sendMessage('Let me just check...', interface, from);
-        var options = {
-            hostname: 'isup.me',
-            path: '/' + params[0],
-            headers: {
-                'user-agent': 'Woodhouse Bot - https://github.com/lukeb-uk/woodhouse'
-            }
-        },
-        data = '';
 
-        var req = http.request(options, function(res) {
-            res.on('data', function (response) {
-                data += String(response);
+    this.name = 'isup';
+
+    this.init = function(){
+        api.listen('is (.+?) up\\?', function(from, interface, params){
+            api.sendMessage('Let me just check...', interface, from);
+            var options = {
+                hostname: 'isup.me',
+                path: '/' + params[0],
+                headers: {
+                    'user-agent': 'Woodhouse Bot - https://github.com/lukeb-uk/woodhouse'
+                }
+            },
+            data = '';
+
+            var req = http.request(options, function(res) {
+                res.on('data', function (response) {
+                    data += String(response);
+                });
+
+                res.on('end', function() {
+                    var reply;
+
+                    if (data.match('It\'s just you')) {
+                        reply = params[0] + ' looks UP to me!'
+                    } else if (data.match('It\'s not just you!')) {
+                        reply = params[0] + ' looks DOWN to me!'
+                    } else if (data.match('doesn\'t look like a site on the interwho')) {
+                        reply = params[0] + ' doesn\'t seem to be a valid domain'
+                    } else {
+                        reply = params[0] + ' returned an error'
+                    }
+
+                    api.sendMessage(reply, interface, from);
+                })
+            })
+
+            req.on('error', function(e) {
+              console.log('problem with request: ' + e.message);
             });
 
-            res.on('end', function() {
-                var reply;
-
-                if (data.match('It\'s just you')) {
-                    reply = params[0] + ' looks UP to me!'
-                } else if (data.match('It\'s not just you!')) {
-                    reply = params[0] + ' looks DOWN to me!'
-                } else if (data.match('doesn\'t look like a site on the interwho')) {
-                    reply = params[0] + ' doesn\'t seem to be a valid domain'
-                } else {
-                    reply = params[0] + ' returned an error'
-                }
-
-                api.sendMessage(reply, interface, from);
-            })
+            req.end();
         })
+    }
 
-        req.on('error', function(e) {
-          console.log('problem with request: ' + e.message);
-        });
-
-        req.end();
-    })
+    return this;
 }
